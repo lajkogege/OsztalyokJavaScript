e1e98a967ebfcab0e2e292d575ec48a6c46c980f
lajkogege <lajkogege@gmail.com>
2024-05-16 15:09:32 +0200

    getter

--- Kutya.js
+++ Kutya.js
@@ -1,35 +1,40 @@
 export default class Kutya {
     //privát adattagok lesznek //az osztályon kivűl nem érhetőek el a privát adattagok.
 
-    #nev=""; //privátáttá teszi a #-t
-    #fajta="";
-    #mondat="";
+    #nev = ""; //privátáttá teszi a #-t
+    #fajta = "";
+    #mondat = "";
     #szuloElem;
 
-    constructor(nev, fajta,mondat, szuloElem){//adattag az osztály tulajdonságai, változoi
+    constructor(nev, fajta, mondat, szuloElem) {//adattag az osztály tulajdonságai, változoi
         /*constructor feladat hogy beálitja az adattagokat */
         //this mindig az osztály aktuális pldányára fog mutatni
-        this.#nev=nev; //publicus adattag, kivűlröl hozzáférhető, megváltoztatható
-        this.#fajta=fajta;
-        this.#mondat=mondat;
-        this.#szuloElem=szuloElem;
+        this.#nev = nev; //publicus adattag, kivűlröl hozzáférhető, megváltoztatható
+        this.#fajta = fajta;
+        this.#mondat = mondat;
+        this.#szuloElem = szuloElem;
         this.#megjelenit();
     }
 
+    /*privát adattagok elérése GETTER-rel*/
+    get nev() {
+        return this.#nev;
+    }
+
     //tagfügvény, az adattagokon végez műveleteket
-    beszel(){
+    beszel() {
         console.log(this.mondat);
     }
 
-    #htmlOsszealit(){//privát, kivűlről nem elérhető
-        let txt=`<div class="kutya">
+    #htmlOsszealit() {//privát, kivűlről nem elérhető
+        let txt = `<div class="kutya">
                     <h2>${this.#nev}</h2>
                     <h2>${this.fajta}</h2>
                 </div>`;
         return txt;
     }
 
-    #megjelenit(){//privát, kivűlről nem elérhető
+    #megjelenit() {//privát, kivűlről nem elérhető
         this.#szuloElem.append(this.#htmlOsszealit());
     }
 
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import Kutya from "./Kutya.js";
 
 //példányositom a kutyát
 
-const ujKutya= new Kutya("Nelson", "golden retrivel", "Adj enni!", $(".tarolo"));
-const ujKutya2= new Kutya("Dézi", "keverék", "Szeretek gazdi!",$(".tarolo"));
+const ujKutya = new Kutya("Nelson", "golden retrivel", "Adj enni!", $(".tarolo"));
+const ujKutya2 = new Kutya("Dézi", "keverék", "Szeretek gazdi!", $(".tarolo"));
 
 //ujKutya.nev="Átnevezem"
 //kivűlről tudok újabb adattagokat hozzáadni az osztályhoz, DE ILYENT INKÁBB NE!!
 //ujKutya.egyEgeszenUjTulajdonsag="Osztályon kivűl is tudok, konstruktor hivás nélkül is értéket adni neki";
 //delete ujKutya.fajta //tudok adattagokat törrölni is, DE ILYENEKET INKÁBB NE!! 
-console.log(ujKutya.nev)
+//console.log(ujKutya.nev)
+//privát adattagokat akarjuk elrni, akkor GETEREKET használunk
 console.log(ujKutya2.nev)
 
 console.log(ujKutya);
